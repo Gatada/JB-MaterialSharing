ac4030ee844a38db4cc834d3f4f49f555c42c815
Johan H. W. Basberg <github@basberg.com>
2023-11-15 15:19:23 +0000

    Pasting now works!
    Yes, this is it. I have not tried it a lot with different materials, but I think it should work.

--- jb-materialsharing.py
+++ jb-materialsharing.py
@@ -1,12 +1,26 @@
+# THE MATERIAL SHARING ADD-ON
+#
+# Initial release was developed and shared November 2023 by Johan Basberg.
+#
+# I hope you this will finally allow us all to easily share materials without
+# having to resort to screenshots.
 # 
+# Future feature: allow me to copy some nodes, not the entire material.
+#
+# Give me a shout-out on Twitter if you find it useful: @johanhwb
+#
+# Enjoy!
+# Johan
+
 
 bl_info = {
 	"author": "Johan Basberg",
-	"version": (0, 3, 23),
+	"version": (0, 3, 47),
 	"name": "Material Sharing using JSON",
 	"blender": (2, 80, 0),
 	"category": "Material",
-	"description": "Copying and pasting materials for easy sharing",
+	"location": "TopBar > Edit",
+	"description": "Copying and pasting active material as JSON for easy sharing.",
 }
 
 import bpy
@@ -24,14 +38,14 @@ class BlenderEncoder(json.JSONEncoder):
 		return super().default(obj)
 
 
-class JB_NODESHARING_OT_save_material_json_to_file(bpy.types.Operator):
+class JB_MATERIALSHARING_OT_save_material_json_to_file(bpy.types.Operator):
 		bl_idname = "jb_nodesharing.save_material"
 		bl_label = "Save Material"
 		bl_options = {'REGISTER', 'UNDO'}
 		
 		def execute(self, context):
-			if bpy.context.active_object:
-				active_material = bpy.context.active_object.active_material
+			if context.active_object:
+				active_material = context.active_object.active_material
 				if active_material:
 					
 					material_json = material_to_json(active_material)
@@ -50,32 +64,32 @@ class JB_NODESHARING_OT_save_material_json_to_file(bpy.types.Operator):
 			return {'FINISHED'}
 
 
-class JB_NODESHARING_OT_copy_material(bpy.types.Operator):
+class JB_MATERIALSHARING_OT_copy_material(bpy.types.Operator):
 	bl_idname = "jb_nodesharing.copy_material"
 	bl_label = "Copy Material"
 	bl_options = {'REGISTER', 'UNDO'}
 
 	def execute(self, context):
 		# Get the active material
-		#active_material = bpy.context.active_object.active_material
+		#active_material = context.active_object.active_material
 		#
 		#if active_material:
 		#	# Store the material as text in a property
-		#	bpy.context.scene.jb_nodesharing_copy_paste = str(active_material)
+		#	context.scene.JB_MATERIALSHARING_copy_paste = str(active_material)
 		#	self.report({'INFO'}, 'Material copied successfully')
 		#else:
 		#	self.report({'WARNING'}, 'No active material to copy')
 		#
 		
 		# Example: Convert the active material to JSON
-		if bpy.context.active_object:
-			active_material = bpy.context.active_object.active_material
+		if context.active_object:
+			active_material = context.active_object.active_material
 			if active_material:
 				
-				material_json = material_to_json(active_material)
+				material_json = self.material_to_json(active_material)
 				
 				json_formatted = json.dumps(material_json, indent=4, cls=BlenderEncoder)
-				bpy.context.window_manager.clipboard = json_formatted
+				context.window_manager.clipboard = json_formatted
 				
 				self.report({'INFO'}, 'Material successfully copied to clipboard.')
 		else:
@@ -83,291 +97,217 @@ class JB_NODESHARING_OT_copy_material(bpy.types.Operator):
 				
 		return {'FINISHED'}
 
-
-def material_to_json(material):
-	# Check if the material uses nodes
-	if material.use_nodes:
-		node_tree = material.node_tree
-		nodes_data = []
-	
-		# Iterate through the nodes in the node tree
-		for node in node_tree.nodes:
-			node_data = {
-				"name": node.name,
-				"type": node.bl_idname,
-				"location": (node.location.x, node.location.y),
-				"inputs": {},
-				"outputs": {}
+		
+	def material_to_json(material):
+		# Check if the material uses nodes
+		if material.use_nodes:
+			node_tree = material.node_tree
+			nodes_data = []
+		
+			# Iterate through the nodes in the node tree
+			for node in node_tree.nodes:
+				node_data = {
+					"name": node.name,
+					"type": node.bl_idname,
+					"location": (node.location.x, node.location.y),
+					"inputs": {},
+					"outputs": {}
+				}
+		
+				# Store information about inputs
+				for input_socket in node.inputs:
+					input_data = {
+						"links": [{"from_node": link.from_node.name, "from_socket": link.from_socket.name} for link in input_socket.links]
+					}
+		
+					# Check if the socket is connected
+					if hasattr(input_socket, "default_value"):
+						input_data["default_value"] = input_socket.default_value
+		
+					node_data["inputs"][input_socket.name] = input_data
+		
+				# Store information about outputs
+				for output_socket in node.outputs:
+					output_data = {
+						"links": [{"to_node": link.to_node.name, "to_socket": link.to_socket.name} for link in output_socket.links]
+					}
+		
+					# Check if the socket is connected
+					if hasattr(output_socket, "default_value"):
+						output_data["default_value"] = output_socket.default_value
+		
+					node_data["outputs"][output_socket.name] = output_data
+		
+				nodes_data.append(node_data)
+		
+			material_data = {
+				"name": material.name,
+				"nodes": nodes_data
 			}
-	
-			# Store information about inputs
-			for input_socket in node.inputs:
-				input_data = {
-					"links": [{"from_node": link.from_node.name, "from_socket": link.from_socket.name} for link in input_socket.links]
-				}
-	
-				# Check if the socket is connected
-				if hasattr(input_socket, "default_value"):
-					input_data["default_value"] = input_socket.default_value
-	
-				node_data["inputs"][input_socket.name] = input_data
-	
-			# Store information about outputs
-			for output_socket in node.outputs:
-				output_data = {
-					"links": [{"to_node": link.to_node.name, "to_socket": link.to_socket.name} for link in output_socket.links]
-				}
-	
-				# Check if the socket is connected
-				if hasattr(output_socket, "default_value"):
-					output_data["default_value"] = output_socket.default_value
-	
-				node_data["outputs"][output_socket.name] = output_data
-	
-			nodes_data.append(node_data)
-	
-		material_data = {
-			"name": material.name,
-			"nodes": nodes_data
-		}
-	
-		return material_data
-	else:
-		return None
+		
+			return material_data
+		else:
+			return None
+
+
+
 
 	
-def create_material_from_json(context, material_data):
-	if not isinstance(material_data, dict):
-		raise ValueError("Invalid material data. Expected a dictionary.")
-		
-	# Check for the presence of required keys
-	required_keys = ["name", "nodes"]
-	for key in required_keys:
-		if key not in material_data:
-			raise ValueError(f"Invalid material format. Missing key '{key}' in material data.")
-	
-	# Check if there's an active object with a material slot
-	if context.active_object and bpy.context.active_object.type == 'MESH' and context.active_object.material_slots:
-		
-		# Create a new material or use the first material slot's material
-		# material = context.active_object.material_slots[0].material
-		material = context.active_object.active_material
-		material.name = material_data.get("name", "Material")
-	
-		nodes_data = material_data.get("nodes", [])
-		node_dict = {}
-	
-		# Create nodes
-		for node_data in nodes_data:
-			node_type = node_data.get("type", "")
-			node_name = node_data.get("name", "")
-			node_location = node_data.get("location", (0, 0))
-	
-			# Use shader.new_node_tree instead of ops.node.add for shader nodes
-			shader_tree = material.node_tree
-			new_node = shader_tree.nodes.new(type=node_type)
-			new_node.location.x = node_location[0]
-			new_node.location.y = node_location[1]
-	
-			node_dict[node_name] = new_node
-	
-		
-		# Connect nodes
-		for link_data in material_data.get("links", []):
-			from_node_name = link_data.get("from_node", "")
-			from_socket_name = link_data.get("from_socket", "")
-			to_node_name = link_data.get("to_node", "")
-			to_socket_name = link_data.get("to_socket", "")
-			
-			from_node = node_dict.get(from_node_name)
-			to_node = node_dict.get(to_node_name)
-			
-			if from_node and to_node:
-				from_socket = from_node.outputs.get(from_socket_name)
-				to_socket = to_node.inputs.get(to_socket_name)
-			
-				if from_socket and to_socket:
-					# Create a new link between nodes without using bpy.ops.node.link
-					material.node_tree.links.new(from_socket, to_socket)
 
-	#	for node_data in nodes_data:
-	#		node_name = node_data.get("name", "")
-	#		node = node_dict.get(node_name)
-	#		
-	#		for output_name, output_data in node_data.get("outputs", {}).items():
-	#			output_socket = node.outputs.get(output_name)
-	#			if output_socket:
-	#				for link_data in output_data.get("links", []):
-	#					to_node_name = link_data.get("to_node", "")
-	#					to_socket_name = link_data.get("to_socket", "")
-	#					to_node = node_dict.get(to_node_name)
-	#					if to_node:
-	#						to_socket = to_node.inputs.get(to_socket_name)
-	#						if to_socket:
-	#							bpy.ops.node.link(type='SHADER', from_socket=output_socket, to_socket=to_node.inputs.get(to_socket_name))
-	#							# material.node_tree.links.new(from_socket, to_socket)
-	#		
-	#		# Set default values for input sockets
-	#		for input_name, input_data in node_data.get("inputs", {}).items():
-	#			input_socket = node.inputs.get(input_name)
-	#			if input_socket:
-	#				default_value = input_data.get("default_value")
-	#				if default_value is not None:
-	#					input_socket.default_value = default_value
-		
-	
-		# Switch back to the 3D View context
-		# bpy.context.area.type = 'VIEW_3D'
-	
-	else:
-		raise ValueError('No active object with material slots found')
 					
 
-class JB_NODESHARING_OT_paste_material_from_clipboard(bpy.types.Operator):
+class JB_MATERIALSHARING_OT_paste_material_from_clipboard(bpy.types.Operator):
 	bl_idname = "jb_nodesharing.paste_material_from_clipboard"
 	bl_label = "Paste Material"
 	bl_options = {'REGISTER', 'UNDO'}
 	
-	def execute(self, context):
-		# Get the stored material text
-		#
-		#	material_text = bpy.context.scene.jb_nodesharing_copy_paste
-		#
-		#	if material_text:
-		#		# Create a new material
-		#		new_material = bpy.data.materials.new(name="Pasted Material")
-		#		new_material.use_nodes = True
-		#		bpy.context.active_object.data.materials.append(new_material)
-		#		bpy.context.active_object.active_material = new_material
-		#
-		#		# Create a new node tree and parse the material text
-		#		tree = new_material.node_tree
-		#		tree.nodes.clear()
-		#		bpy.ops.node.read_file(filepath="", files=[{"name": "Material Nodes", "content": material_text}])
-		#
-		#		self.report({'INFO'}, 'Material pasted successfully')
-		#	else:
-		#		self.report({'WARNING'}, 'No material text to paste')
-		
-		# active_object = bpy.context.active_object
-		# if active_object is not None and active_object.active_material is not None:
-		# 	active_material = active_object.active_material
-		# 	create_material_from_json(material_data)
-		# else:
-		# 	print("No active object or active material.")
-		
-		# Get the JSON string from the clipboard in Blender
-		json_string_from_clipboard = bpy.context.window_manager.clipboard
-		
-		try:
+	def execute(self, context):		
+		active_object = context.active_object
+		if active_object is not None and active_object.active_material is not None:
+
+			# Get the JSON string from the clipboard in Blender
+			json_string_from_clipboard = context.window_manager.clipboard
+			
+			
 			# Attempt to parse JSON string representing material data
 			material_data_from_clipboard = json.loads(json_string_from_clipboard)
 			
 			# Call the function to create material from the retrieved data
 			create_material_from_json(context, material_data_from_clipboard)
 			
-			return {'FINISHED'}
-					
-		except json.JSONDecodeError:
-			# Handle the case where JSON parsing fails
-			self.report({'WARNING'}, 'Invalid material format on Clipboard')
+			try:
+				# Attempt to parse JSON string representing material data
+				material_data_from_clipboard = json.loads(json_string_from_clipboard)
+				
+				# Call the function to create material from the retrieved data
+				self.create_material_from_json(context, material_data_from_clipboard)
+				
+				self.report({'INFO'}, 'Material was successfully pasted!')
+				
+				return {'FINISHED'}
+						
+			except json.JSONDecodeError:
+				# Handle the case where JSON parsing fails
+				self.report({'WARNING'}, 'Invalid material format on Clipboard')
+				return {'CANCELLED'}
+				
+			except ValueError as ve:
+				self.report({'WARNING'}, f"Error creating material: {ve}")
+				return {'CANCELLED'}
+				# Handle the specific ValueError, such as displaying a user-friendly error message
+			
+			except Exception as e:
+				self.report({'WARNING'}, f"Unexpected error: {e}")
+				print(e)
+				return {'CANCELLED'}
+
+		else:
+			self.report({'WARNING'}, 'No active object or active material. Select an object with a material')
 			return {'CANCELLED'}
 			
-		except ValueError as ve:
-			self.report({'WARNING'}, f"Error creating material: {ve}")
-			return {'CANCELLED'}
-			# Handle the specific ValueError, such as displaying a user-friendly error message
+			
+	def create_material_from_json(context, material_data):
+		if not isinstance(material_data, dict):
+			raise ValueError("Invalid material data. Expected a dictionary.")
+			
+		# Check for the presence of required keys
+		required_keys = ["name", "nodes"]
+		for key in required_keys:
+			if key not in material_data:
+				raise ValueError(f"Invalid material format. Missing key '{key}' in material data.")
 		
-		except Exception as e:
-			self.report({'WARNING'}, f"Unexpected error: {e}")
-			return {'CANCELLED'}
-
-	def parse_material(self, material):
-		# Print basic information about the material
-		print(f"Material Name: {material.name}")
-		print(f"Use Nodes: {material.use_nodes}")
-		
-		# Access the material nodes
-		if material.use_nodes:
-			node_tree = material.node_tree
-			nodes = node_tree.nodes
-
-			self.print_node_connections(node_tree)
-		
-			# Print information about each node in the material
-			for node in nodes:
-				print("=" * 30)
-				print(f"Node Type: {node.type}")
-				print(f"Node Name: {node.name}")
-				print(f"Node: {node}")
+		# Check if there's an active object with a material slot
+		if context.active_object and context.active_object.type == 'MESH' and context.active_object.material_slots:
+			
+			# Clear all nodes in the node tree
+			context.active_object.active_material.node_tree.nodes.clear()
+			
+			# Create a new material or use the first material slot's material
+			# material = context.active_object.material_slots[0].material
+			bpy.ops.material.new()
+			material = context.active_object.active_material
+			material.name = material_data.get("name", "Material")
+			
+			nodes_data = material_data.get("nodes", [])
+			node_dict = {}
+			
+			# Create nodes
+			for node_data in nodes_data:
+				node_type = node_data.get("type", "")
+				node_name = node_data.get("name", "")
+				node_location = node_data.get("location", (0, 0))
+			
+				# Use shader.new_node_tree instead of ops.node.add for shader nodes
+				shader_tree = material.node_tree
+				new_node = shader_tree.nodes.new(type=node_type)
+				new_node.location.x = node_location[0]
+				new_node.location.y = node_location[1]
 				
-				self.parse_properties(node)
-				
-				print(f"Location: {node.location}\n\n")
+				node_dict[node_name] = new_node
+			
+			# Connect nodes
+			for node_data in nodes_data:
+				from_node_name = node_data.get("name", "")
+				from_node = node_dict[from_node_name]
+			
+				for input_name, input_data in node_data.get("inputs", {}).items():
+					input_socket = from_node.inputs.get(input_name)
+					if input_socket:
+						for link_data in input_data.get("links", []):
+							to_node_name = link_data.get("from_node", "")
+							to_socket_name = link_data.get("from_socket", "")
+							to_node = node_dict.get(to_node_name)
+							if to_node:
+								to_socket = to_node.outputs.get(to_socket_name)
+								if to_socket:
+									bpy.context.active_object.active_material.node_tree.links.new(input_socket, to_socket)	
+			
+			# Set default values
+			for node_data in nodes_data:
+				node_name = node_data.get("name", "")
+				node = node_dict.get(node_name)
+			
+				# Set default values for input sockets
+				for input_name, input_data in node_data.get("inputs", {}).items():
+					input_socket = node.inputs.get(input_name)
+					if input_socket:
+						default_value = input_data.get("default_value")
+						if default_value is not None:
+							input_socket.default_value = default_value
 		
-				#	# Additional properties depending on the node type
-				#	if node.type == 'BSDF_PRINCIPLED':
-				#		print(f"Base Color: {node.base_color}")
-				#		print(f"Metallic: {node.metallic}")
-				#		print(f"Roughness: {node.roughness}")
-				#	elif node.type == 'ShaderNodeTexImage':
-				#	print(f"Image: {node.image}")
+			# Switch back to the 3D View context
+			# context.area.type = 'VIEW_3D'
 		
-		return {'FINISHED'}
-	
-	def print_node_connections(self, node_tree):
-		print("-" * 30)
-		print("\n\nNODE CONNECTIONS")
-		for link in node_tree.links:
-			print(f"From Node: {link.from_node.name}")
-			print(f"From Socket: {link.from_socket.name}")
-			print(f"To Node: {link.to_node.name}")
-			print(f"To Socket: {link.to_socket.name}")
-			print("-" * 30)
-		return {'FINISHED'}
+		else:
+			raise ValueError('No active object with material slots found')
 
-		
-	def parse_properties(self, bpy_struct):
-		for prop_name, prop_info in bpy_struct.bl_rna.properties.items():
-			# Check if the property is valid for the current context
-			if bpy_struct.is_property_hidden(prop_name) or not bpy_struct.is_property_set(prop_name):
-				continue
-		
-			# Access the property value
-			prop_value = getattr(bpy_struct, prop_name)
-		
-			print(f"Property Name: {prop_name}")
-			print(f"Property Type: {prop_info.type}")
-			print(f"Property Value: {prop_value}")
-			print("=" * 30)
-		
-		return {'FINISHED'}
-		
 
 classes = (
-	JB_NODESHARING_OT_save_material_json_to_file,
-	JB_NODESHARING_OT_copy_material,
-	JB_NODESHARING_OT_paste_material_from_clipboard
+	JB_MATERIALSHARING_OT_save_material_json_to_file,
+	JB_MATERIALSHARING_OT_copy_material,
+	JB_MATERIALSHARING_OT_paste_material_from_clipboard
 )
 
 def menu_func(self, context):
-	self.layout.operator(JB_NODESHARING_OT_save_material_json_to_file.bl_idname)
-	self.layout.operator(JB_NODESHARING_OT_copy_material.bl_idname)
-	self.layout.operator(JB_NODESHARING_OT_paste_material_from_clipboard.bl_idname)
+	self.layout.separator()
+	self.layout.operator(JB_MATERIALSHARING_OT_save_material_json_to_file.bl_idname)
+	self.layout.separator()
+	self.layout.operator(JB_MATERIALSHARING_OT_copy_material.bl_idname)
+	self.layout.operator(JB_MATERIALSHARING_OT_paste_material_from_clipboard.bl_idname)
 
 def register():
 	for cls in classes:
 		bpy.utils.register_class(cls)
 	bpy.types.TOPBAR_MT_edit.append(menu_func)
 
-	bpy.types.Scene.jb_nodesharing_copy_paste = bpy.props.StringProperty(default="")
+	bpy.types.Scene.JB_MATERIALSHARING_copy_paste = bpy.props.StringProperty(default="")
 
 def unregister():
 	for cls in classes:
 		bpy.utils.unregister_class(cls)
 	bpy.types.TOPBAR_MT_edit.remove(menu_func)
 
-	del bpy.types.Scene.jb_nodesharing_copy_paste
+	del bpy.types.Scene.JB_MATERIALSHARING_copy_paste
 
 if __name__ == "__main__":
 	register()
